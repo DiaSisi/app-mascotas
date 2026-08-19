6e2dfa00eaf58b83f06aacac6a5b66757e0200d0
Diana Gomes Sisiruca <dianagsisiruca@gmail.com>
2026-08-19 17:03:59 -0400

    style: actualizar diseño del calendario y zona horaria

--- app_mascotas/admin.py
+++ app_mascotas/admin.py
@@ -1,7 +1,26 @@
 # Register your models here.
 from django.contrib import admin
+from django.db import models
+from django import forms
 from .models import Mascota, RegistroMedico, Suscripcion
 
-admin.site.register(Mascota)
-admin.site.register(RegistroMedico)
-admin.site.register(Suscripcion)
+# Forzamos el uso del calendario moderno de HTML5
+class MascotaAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.DateField: {'widget': forms.DateInput(attrs={'type': 'date'})},
+    }
+
+class RegistroMedicoAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.DateField: {'widget': forms.DateInput(attrs={'type': 'date'})},
+    }
+
+class SuscripcionAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.DateField: {'widget': forms.DateInput(attrs={'type': 'date'})},
+    }
+
+# Registramos TODOS los modelos con su nueva configuración
+admin.site.register(Mascota, MascotaAdmin)
+admin.site.register(RegistroMedico, RegistroMedicoAdmin)
+admin.site.register(Suscripcion, SuscripcionAdmin)
