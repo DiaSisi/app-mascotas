a00348625bfdd8760c696d36fada14a4f466c0ef
Diana Gomes Sisiruca <dianagsisiruca@gmail.com>
2026-08-19 11:26:59 -0400

    fix: auto-crear superusuario y agregar banner de credenciales

--- app_mascotas/views.py
+++ app_mascotas/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import Mascota
 
 def dashboard(request):
-    # Traemos todas las mascotas de la base de datos
+    # TRUCO: Crear el usuario evaluador automáticamente si no existe en Render
+    if not User.objects.filter(username='profesor').exists():
+        User.objects.create_superuser('profesor', 'profe@correo.com', 'diplomado123')
+        
     mascotas = Mascota.objects.all()
-    # Las enviamos al archivo HTML
     return render(request, 'index.html', {'mascotas': mascotas})
